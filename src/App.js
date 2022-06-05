1a1160045b46e9292cf4abef7c40e1595b512765
tohoilam <61353084+tohoilam@users.noreply.github.com>
2022-06-05 11:04:51 +0800

    Basic Login Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import React from 'react';
 import './App.css';
+import { getTokenFromUrl } from './api/spotify_connection';
+import SpotifyApi from './api/spotify_api';
+
 import Login from './components/Login/Login';
+import Home from './components/Home/Home';
 
-function App() {
-  return (
-    <Login />
-  );
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      spotifyApi: new SpotifyApi(),
+      token: "",
+    }
+  }
+
+  // Mount after collecting the token
+  componentWillMount() {
+    const response = getTokenFromUrl()
+    window.location.hash = ""; // Hide token in url bar
+    const _token = response.access_token;
+
+    if (_token) {
+      this.setState({token: _token});
+      this.state.spotifyApi.setAccessToken(_token);
+    }
+  }
+
+  render() {
+    return (
+      <div className="">
+        { // if token exist, go to main page, if not, go to login page
+          this.state.token !== "" ? (
+            <div>
+              <Home spotifyApi={this.state.spotifyApi}/>
+            </div>
+          ) : (
+            <Login />
+          )
+        }
+        
+      </div>
+    );
+  }
 }
-
-export default App;
